7c305a510206ec8a41d3a38dc5f8fc8f72286e21
Ryu613 <411379453@qq.com>
2026-07-03 12:54:54 +0800

    test shader compiler

--- cmake/CompileShaders.cmake
+++ cmake/CompileShaders.cmake
@@ -1,4 +1,20 @@
 # use to compile shaders
-function(PD2CompileShader target shader output stage)
-# TODO
+function(CompileShader lang target shader output)
+set(shaderDir "${PD2_ROOT_DIR}/assets/shaders/")
+if(lang STREQUAL "SLANG")
+    find_program(SLANGC_EXECUTABLE slangc REQUIRED)
+    add_custom_command(
+        OUTPUT ${output}
+        DEPENDS ${PD2_ROOT_DIR}/assets/shaders/${shader}
+        COMMAND ${SLANGC_EXECUTABLE} ${shaderDir}${shader} -target spirv -profile spirv_1_4 -entry vertMain -entry fragMain -o ${shaderDir}${output}
+        COMMENT "Compile Slang shader ${shader} using slangc"
+    )
+    add_custom_target(
+        ${target}Shaders ALL
+        DEPENDS ${output}
+        COMMENT "Target to compile a slang shader"
+    )
+else()
+    message(FATAL_ERROR "SHADER LANGUAGE NOT SUPPORTED!")
+endif()
 endfunction()
